950164cd37daf8eabc2551fbeb67de7f5153ede9
Michael Kilmer <mchlkilmer@hotmail.com>
2017-03-07 15:33:36 -0800

    Update Solar.js

--- Assignment_5/Solar.js
+++ Assignment_5/Solar.js
@@ -21,7 +21,7 @@ var Planets = {
   // Mercury : undefined,
   // Venus : undefined,
   Earth : undefined,
-  // Moon : undefined,
+  Moon : undefined,
   // Mars : undefined,
   // Jupiter : undefined,
   // Saturn : undefined,
@@ -156,6 +156,26 @@ function render() {
   ms.push();
   ms.rotate(data.year*time, [0,0,1]);
   ms.translate(data.distance*10, 0, 0);
+  
+  ms.push();
+  ms.scale(data.radius);
+  ms.pop();
+  
+  gl.useProgram(planet.program);
+  gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
+  gl.uniformMatrix4fv(planet.uniforms.P, false, flatten(P));
+  gl.uniform4fv(planet.uniforms.color, flatten(data.color));
+  planet.render();
+  
+  name = "Moon";
+  planet = Planets[name];
+  data = SolarSystem[name];
+  
+  planet.PointMode = false;
+  
+  ms.push();
+  ms.rotate(data.year*time, [0,0,1]);
+  ms.translate(data.distance*10, 0, 0);
   ms.scale(data.radius);
   gl.useProgram(planet.program);
   gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
@@ -163,6 +183,7 @@ function render() {
   gl.uniform4fv(planet.uniforms.color, flatten(data.color));
   planet.render();
   ms.pop();
+  ms.pop(); //pop back to pre earth status
 
   window.requestAnimationFrame(render);
 }
