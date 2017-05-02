502394f401ac04ceac39056f8d3f594dc82fae1d
Michael Kilmer <mchlkilmer@hotmail.com>
2017-05-01 19:51:27 -0700

    Update firework.js

--- Final Project/firework.js
+++ Final Project/firework.js
@@ -1,1 +1,13 @@
 
+function Firework(){
+  this.firework = new Particle(random(width),height);
+  
+  this.update = function(){
+    firework.applyForce(gravity);
+    firework.update();
+  }
+  
+  this.show = function(){
+    firework.show();
+  }
+}
