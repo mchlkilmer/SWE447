efb61c3672a26f8fdb03a97e89d3fca6b5b9852d
Michael Kilmer <mchlkilmer@hotmail.com>
2017-05-01 20:46:19 -0700

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -3,6 +3,7 @@ var gravity;
 
 function setup(){
   createCanvas(400,300);
+  colorMode(HSB);
   gravity = createVector(0,0.2);
  
   stroke(255);
@@ -11,7 +12,7 @@ function setup(){
 }
 
 function draw () {
-  background(0 , 25);
+  background(0 , 0, 0, 25);
   
   if(random(1) < 0.03){
    fireworks.push(new Firework());
