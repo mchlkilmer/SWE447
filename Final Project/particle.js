6fdc7812abc85cca223a9f615f6f9c0bfb8e1c41
Michael Kilmer <mchlkilmer@hotmail.com>
2017-05-01 20:27:57 -0700

    Update particle.js

--- Final Project/particle.js
+++ Final Project/particle.js
@@ -2,11 +2,13 @@
 
 function Particle(x,y, firework) {
   this.pos = createVector(x,y);
+  this.firework = firework;
   if(firework){
     this.vel = createVector(0, random(-12, -8));
   }
   else{
     this.vel = p5.Vector.random2D();
+    this.vel.mult(random(1,6));
   }
   this.acc = createVector(0,0);
   
@@ -16,6 +18,10 @@ function Particle(x,y, firework) {
   
   
   this.update = function() {
+    if (!this.firework){
+      this.vel.mult(0.85);
+    }
+      
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
