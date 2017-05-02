9958decdb3e2aa7e7b5d66363c21487f637aeca5
Michael Kilmer <mchlkilmer@hotmail.com>
2017-05-01 19:51:25 -0700

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,17 +1,18 @@
-var firework;
+var fireworks = [];
 var gravity;
 
 function setup(){
   createCanvas(400,300);
   gravity = createVector(0,0.2);
+  fireworks.push(new Firework());
   stroke(255);
   strokeWeight(4);
-  firework = new Particle(random(width),height);
 }
 
 function draw () {
   background(51);
-  firework.applyForce(gravity);
-  firework.update();
-  firework.show();
+  for( var i = 0; i < fireworks.length; i++){
+    fireworks[i].update();
+    fireworks[i].show();
+  }
 }
